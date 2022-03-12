12e6d7d5b4bf31d16f1d9f78b2e02f9ecd6e6976
Sainath1998 <97929522+Sainath1998@users.noreply.github.com>
2022-03-12 22:50:12 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,10 @@
+num1 = int(input('enter a number \n'))
+num2 = int(input('enter another number \n'))
+sum = num1+num2
+diffrence = num1 - num2
+division = float (num1/ num2)
+product = num1*num2
+print(sum)
+print(diffrence)
+print(division)
+print(product)
