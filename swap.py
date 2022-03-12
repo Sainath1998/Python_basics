672e48768aeba9c9b8c7a2555055f689e2dfedd0
Sainath1998 <97929522+Sainath1998@users.noreply.github.com>
2022-03-12 23:13:30 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,11 @@
+p = int(input('enter a number\n'))
+q = int(input('enter another number\n'))
+print('Before swap')
+print('p=',p)
+print('q=',q)
+temp = q
+q = p
+p = temp
+print('After swap')
+print('p=',p)
+print('q=',q)
